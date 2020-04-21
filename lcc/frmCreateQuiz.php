a8c0c2edd30085f6add9e5bfab48e57fee8cc7a1
MichaelJunges <michaeljunges70@gmail.com>
2020-04-21 01:14:39 -0300

    Add pergunta

--- lcc/frmCreateQuiz.php
+++ lcc/frmCreateQuiz.php
@@ -29,6 +29,11 @@ $id = $_GET["id"];
 	 		<h1 class="titulo">CRIE SEU QUIZ</h1>
 
 	 		<p class="quiz"> 
+				Pergunta:<br /> 
+				<input  type="text" size="50" name="pergunta" required="required" maxlength="100" autofocus="autofocus" > 
+			</p> 	
+
+	 		<p class="quiz"> 
 				Correta:<br /> 
 				<input  type="text" size="30" name="correta" required="required" maxlength="20" autofocus="autofocus" > 
 			</p> 	
@@ -40,12 +45,12 @@ $id = $_GET["id"];
 
 			<p class="quiz"> 
 				Errada:<br /> 
-				<input  type="text" size="30" name="errada" required="required" maxlength="20" autofocus="autofocus" > 
+				<input  type="text" size="30" name="errada2" required="required" maxlength="20" autofocus="autofocus" > 
 			</p>
 
 			<p class="quiz"> 
 				Errada:<br /> 
-				<input  type="text" size="30" name="errada" required="required" maxlength="20" autofocus="autofocus" > 
+				<input  type="text" size="30" name="errada3" required="required" maxlength="20" autofocus="autofocus" > 
 			</p>
 
 			<input type="hidden" name="id" value="<?=$id?>">
