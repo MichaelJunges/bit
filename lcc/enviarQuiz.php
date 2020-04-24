404c537499a22951c2514ffc71f252fd561ad595
MichaelJunges <michaeljunges70@gmail.com>
2020-04-24 14:12:56 -0300

    Att

--- lcc/enviarQuiz.php
+++ lcc/enviarQuiz.php
@@ -15,19 +15,34 @@ if($_SESSION["acesso"] != true) {
 	$errada2 = $_POST["errada2"];
 	$errada3 = $_POST["errada3"];
 	$id_usuario = $_POST["id"];
-	$local = $_FILES["foto"]["tmp_name"];
-	$foto = $_FILES['foto']["name"];
+
+	//foto
+	$foto = $_POST["foto"];
 	$date = date('Y-m-d.H.i.s');
 	$conteudo = "fotos_quiz/$date$foto";
-	copy($local, $conteudo);
+
+	$targ_w = $_POST['w'];
+    $targ_h = $_POST['h'];
+    $jpeg_quality = 90;
+
+    $src = $_POST['imagem'];
+    $img_r = imagecreatefromjpeg($src);
+    $dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
+
+    imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'],$targ_w,$targ_h,$_POST['w'],$_POST['h']);
+    imagejpeg($dst_r, $conteudo, $jpeg_quality);
+
+	unlink("fotos_quiz/$foto");
+	//fim foto
 
 	$sqlInsert = $PDO->prepare("INSERT INTO quiz (titulo, pergunta, correta, errada, errada2, errada3, id_usuario, foto) VALUES (?,?,?,?,?,?,?,?)");
 	$exec = $sqlInsert->execute(array($titulo, $pergunta, $correta, $errada, $errada2, $errada3, $id_usuario, $conteudo));
 
 	if ($exec){
-		$msg=urlencode("Sucesso");
+		$msg=urlencode("Quiz criado com sucesso");
 		header("Location:home.php?msg=$msg");
   	}else{
-  		$msg=urlencode("Erro");
+  		$msg=urlencode("Falha ao criar quiz, por favor tente novamente");
     	header("Location:home.php?msg=$msg");
   	}
+?>
--- lcc/frmCreateQuiz.php
+++ lcc/frmCreateQuiz.php
@@ -2,83 +2,55 @@
 session_start();
 
 if($_SESSION["acesso"] != true) {
-
 	$mensagem = urldecode("Erro");
 	header("Location:index.php?msg=$mensagem");
 	exit;
 }
-
 $PDO = new PDO("sqlite:users.db");
 $id = $_SESSION["id"];
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" href="css/style1.css?time=<?=time()?>">
-	 <link rel="stylesheet" href="css/unsemantic-grid-responsive.css">
-	 <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-	 <script  src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/unsemantic-grid-responsive.css">
+	<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-	 <script>
-	 	function imagem(){
-			location.href="recortarImagem.php"
-	 	}
-	 </script>
+    <script>
+    	function readURL(input) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        $('#ImageCrop').attr('src', e.target.result)
+	    };
+	    reader.readAsDataURL(input.files[0]);
+	} 
+	function mostraImagem(){
+	    $('input[type=file]').each(function(index){
+	        if ($('input[type=file]').eq(index).val() != ""){
+	            readURL(this);
+	        }
+	    });
+	}
+    </script>
 </head>
 <body style="font-family: 'Press Start 2P', cursive;">
-	<form name="frmCreateQuiz" action="enviarQuiz.php" method="POST" enctype="multipart/form-data" >
-
-	 <div class="grid-container">
-	 	<div class="grid-100">
-	 		<div class="grid-50"></div>
-	 		<h1 class="titulo">CRIE SEU QUIZ</h1>
-	 		<p class="quiz">
-				Foto:<br />
-				<input name="foto" type="file" accept=".jpg, .jpeg" required="required">
-			</p><br>
-	 		<p class="quiz"> 
-				Titulo:<br /> 
-				<input  type="text" size="50" name="titulo" required="required" maxlength="50" autofocus="autofocus" > 
-			</p> 
-	 		<p class="quiz"> 
-				Pergunta:<br /> 
-				<input  type="text" size="50" name="pergunta" required="required" maxlength="100" > 
-			</p> 	
-
-	 		<p class="quiz"> 
-				Correta:<br /> 
-				<input  type="text" size="30" name="correta" required="required" maxlength="20"> 
-			</p> 	
-
-			<p class="quiz"> 
-				Errada:<br /> 
-				<input  type="text" size="30" name="errada" required="required" maxlength="20"> 
-			</p>
-
-			<p class="quiz"> 
-				Errada:<br /> 
-				<input  type="text" size="30" name="errada2" required="required" maxlength="20"> 
-			</p>
-
-			<p class="quiz"> 
-				Errada:<br /> 
-				<input  type="text" size="30" name="errada3" required="required" maxlength="20"> 
-			</p>
-
-			<input type="hidden" name="id" value="<?=$id?>">
-
-			<p class="titulo">  
-				<input type="submit" value="PRÓXIMO" id="proximo">
-			</p>	
-
-</form>
-	 	</div>
-	 	</div>
-	 </div>
+	<div class="grid-container">
+		<div class="grid-100">
+			<div class="grid-50"></div>
+			<h1 class="titulo">CRIE SEU QUIZ</h1>
+			<form name="frmCreateQuiz" action="frmCrQuiz.php" method="POST" enctype="multipart/form-data">
+			<p class="quiz">
+		        Foto:<br />
+		        <input name="foto" type="file" accept=".jpg, .jpeg" required="required" onchange="mostraImagem()">
+		    </p><br>
+		    <p><img id="ImageCrop" width="50%"></p>
+		    <p class="titulo">  
+		        <input type="submit" value="PRÓXIMO" id="proximo">
+		    </p>
+		    </form>
+    	</div>
+    </div>
 </body>
-		
-	
-
 </html>
