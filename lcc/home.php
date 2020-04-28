2d22082015f296d6ed9726a012811c01db385059
MichaelJunges <michaeljunges70@gmail.com>
2020-04-28 10:34:49 -0300

    Homer Simpson

--- lcc/home.php
+++ lcc/home.php
@@ -4,15 +4,13 @@ session_start();
 
 if ($_SESSION["acesso"] != true)
 {
-		// Não está autenticado
+	// Não está autenticado
 	$mensagem = urlencode("Você precisa fazer login!");
 	header("Location:index.php?msg=$mensagem");
 	exit;
 }
-
-
+//Conexão com o banco de dados
 $PDO = new PDO("sqlite:users.db");
-
 
 $login = $_SESSION['login'];
 $id = $_SESSION['id'];
@@ -32,6 +30,7 @@ $sqlUser = $PDO->prepare("SELECT * FROM register WHERE id = ?");
 $sqlUser->execute(array($id));
 $dados = $sqlUser->fetchAll();
 
+//Mensagens enviadas de outras telas
 @$mensagem=urldecode($_GET["msg"]);
 ?>
 
@@ -56,60 +55,54 @@ $dados = $sqlUser->fetchAll();
 	</script>
 </head>
 <body style=" margin: 0px; padding: 0px; background-color: rgb(51,109,226)">
-	
-
 	<div class="grid-container">
-<div class="grid-100">
+	  <div class="grid-33 mobile-grid-100" style="padding: 0px;">
+		<a href="frmEditar.php" style="text-decoration: none;">
+		  <p class="criarEditar">Editar Conta</p>
+		</a>
+	  </div>
 
-<div class="grid-33" style="padding: 0px;">
-<a href="frmEditar.php" style="text-decoration: none;">
-<div class="grid-100 criarEditar" >	
-<p >Editar Conta</p>
-</div>
-</a>
-</div>
+	  <div class="grid-33 mobile-grid-100" style="padding: 0px;">
+		<a href="frmCreateQuiz.php" style="text-decoration: none;">
+		  <p class="criarEditar">Criar Quiz</p>
+		</a>
+	  </div>
 
-<div class="grid-33" style="padding: 0px;">
-<a href="frmCreateQuiz.php" style="text-decoration: none;">
-<div class="grid-100 criarEditar" >	
-<p>Criar Quiz</p>
-</div>
-</a>
-</div>
+	  <div class="grid-33 mobile-grid-100" style="padding: 0px;">
+	    <a href="logoff.php" style="text-decoration: none;">
+		  <p class="criarEditar">Sair</p>
+		</a>
+	  </div>
 
-<div class="grid-33" style="padding: 0px;">
-<a href="logoff.php" style="text-decoration: none;">
-<div class="grid-100 criarEditar" >	
-<p >Sair</p>
-</div>
-</a>
-</div>
+	  <div class="grid-100 about_me">
+ 		<a href="frmFoto.php?id=<?=$id?>">
+ 			<img  width="4%" style="border-radius: 10px; padding-bottom: 0px; padding-right: 10px;" align="left" src="fotos/<?=$id?>.jpg"  >
+ 		</a>
+		<p class="minhasInfo" >Nome: <?=$dados[0]["nreal"]?></p>
+		<p class="minhasInfo">Bio: <?=$dados[0]["bio"]?></p>	
+	  </div>
 
-
-</div>
-
-	<div class="grid-100 about_me">
-
-<div class="grid-60"> 
- <a href="frmFoto.php?id=<?=$id?>"><img  width="6%" style="border-radius: 10px; padding-bottom: 0px; padding-right: 10px;" align="left" src="fotos/<?=$id?>.jpg"  > </a>
-    <p class="minhasInfo" >Nome: <?=$dados[0]["nreal"]?></p>
-     <p class="minhasInfo">Bio: <?=$dados[0]["bio"]?></p>
-</div>	
-</div>
-<p><?=@$mensagem?></p>
-&nbsp
-		<div class="grid-100 people_title" style="padding: 0px">
-			<?php  
-			foreach ($dadosQuiz as $quizzes) {	
-				?>
-				<a href="frmQuizId.php?id=<?=$quizzes["id"]?>&pergunta=0"><div class="grid-25 people" > 
-					 <p class="textoUpImagem" style="text-decoration: none; color: white"> <?=$quizzes["titulo"]?></p>
-
-						<p><img  style="border-radius: 10px" width="100%" height="100%" src="<?=$quizzes["foto"]?>"></p>
-				</div></a>
-				<?php
-			}
-			?>
-</div>
+	  <div class="grid-100">&nbsp</div>
+	  <div class="grid-100"><p class="quiz"><?=@$mensagem?></p></div>
+	  <div class="grid-100">&nbsp</div>
+	  <div class="grid-100 people_title" style="padding: 0px">
+		<?php  
+		  foreach ($dadosQuiz as $quizzes) {	
+		?>
+		  <a href="frmQuizId.php?id=<?=$quizzes["id"]?>&pergunta=0">
+		  	<div class="grid-25 mobile-grid-100 people" > 
+				<p class="textoUpImagem" style="text-decoration: none; color: white">
+				  <?=$quizzes["titulo"]?>		
+				</p>
+				<p>
+				  <img  style="border-radius: 10px" width="100%" height="100%" src="<?=$quizzes["foto"]?>">
+				</p>
+			</div>
+		  </a>
+		<?php
+		  }
+		?>
+	  </div>
+	</div>
 </body>
 </html>
