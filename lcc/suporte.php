e18d058cc64355a8c60f699b578624b96531e822
LuanCastoldi <castoldiluan@gmail.com>
2020-04-30 17:06:15 -0300

    gifs

---
+++ lcc/suporte.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Suporte - 4Quiz</title>
+	<link rel="stylesheet" href="css/login.css?time=<?=time()?>">
+	<link rel="stylesheet" href="css/unsemantic-grid-responsive.css"> 
+</head>
+<body>
+	<div class="grid-container">
+		<div class="grid-100" style="text-align: center;">
+
+		 <a href="home.php">Voltar no 4Quiz </a>	
+
+		<h1>Como Registrar ?</h1>
+		<img src="./icons/registro.gif" alt="gifRegistro" style="border-color: purple; border-style: groove;">
+
+		<h1>Como Responder ao Quiz ?</h1>
+		<img src="./icons/tuto.gif" alt="gifResponde" style="border-color: purple; border-style: groove;">
+
+
+&nbsp
+      
+		</div>
+</div>
+</body>
+</html>
